590f40221501201f3699d9e4650bd99e54df8140
Katerina S. <catarena@gmail.com>
2026-02-14 18:57:04 +0300

    [Этап: Настройка инфраструктуры] Выполнен шаг: Настроен Alembic для миграций БД

---
+++ backend/alembic/versions/20260214185500_initial_schema.py
@@ -0,0 +1,33 @@
+"""Initial schema
+
+Revision ID: 20260214185500
+Revises: 
+Create Date: 2026-02-14 18:55:00.000000
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '20260214185500'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """
+    Placeholder for initial schema migration
+    Will be populated when models are created
+    """
+    pass
+
+
+def downgrade() -> None:
+    """
+    Placeholder for downgrade
+    """
+    pass
