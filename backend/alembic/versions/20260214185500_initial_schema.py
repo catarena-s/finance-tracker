a4ba4597e65f192058826fd72226019d3461e0f0
Katerina S. <catarena@gmail.com>
2026-02-14 19:13:40 +0300

    [Этап: Настройка инфраструктуры] Исправлены ошибки линтинга и CI конфигурация

--- backend/alembic/versions/20260214185500_initial_schema.py
+++ backend/alembic/versions/20260214185500_initial_schema.py
@@ -7,9 +7,6 @@ Create Date: 2026-02-14 18:55:00.000000
 """
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
 
 # revision identifiers, used by Alembic.
 revision: str = '20260214185500'
