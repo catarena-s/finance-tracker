5aa9f8c84a79e338529ebef632a92ba82e558120
Katerina S. <catarena@gmail.com>
2026-02-15 17:13:46 +0300

    feat(frontend): реализованы компоненты транзакций
    - Создан TransactionCard с отображением суммы, категории, даты и описания
    - Создан TransactionList с пагинацией, скелетонами и пустым состоянием
    - Создан TransactionFilters с фильтрацией по типу, категории и датам
    - Создан TransactionForm с валидацией через React Hook Form
    - Все компоненты адаптивны и доступны
    - Цветовое кодирование доходов (зеленый) и расходов (красный)
    
    Шаг 8 завершен

---
+++ frontend/src/components/transactions/index.ts
@@ -0,0 +1,5 @@
+export { TransactionCard } from './TransactionCard';
+export { TransactionList } from './TransactionList';
+export { TransactionFilters } from './TransactionFilters';
+export { TransactionForm } from './TransactionForm';
+export type { TransactionFilterValues, TransactionFormData } from './TransactionFilters';
